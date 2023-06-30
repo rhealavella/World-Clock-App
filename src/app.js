7dcbbb5b6b3835c650573abdeaee0084e355c194
rhealavella <rmdeosaran@gmail.com>
2023-06-29 20:53:04 -0400

    Set Interval added to display seconds

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
-let newYorkDateElement = document.querySelector("#new-york-date");
-newYorkDateElement.innerHTML = moment()
-  .tz("America/New_York")
-  .format("ddd, MMM Do YYYY");
+function updateTime() {
+  let newYorkDateElement = document.querySelector("#new-york-date");
+  newYorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("ddd, MMM Do YYYY");
 
-let newYorkTimeElement = document.querySelector("#new-york-time");
-newYorkTimeElement.innerHTML = moment()
-  .tz("America/New_York")
-  .format("hh:mm A");
+  let newYorkTimeElement = document.querySelector("#new-york-time");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss A");
 
-let parisDateElement = document.querySelector("#paris-date");
-parisDateElement.innerHTML = moment()
-  .tz("Europe/Paris")
-  .format("ddd, MMM Do YYYY");
+  let parisDateElement = document.querySelector("#paris-date");
+  parisDateElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("ddd, MMM Do YYYY");
 
-let parisTimeElement = document.querySelector("#paris-time");
-parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("hh:mm A");
+  let parisTimeElement = document.querySelector("#paris-time");
+  parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("hh:mm:ss A");
 
-let vancouverDateElement = document.querySelector("#vancouver-date");
-vancouverDateElement.innerHTML = moment()
-  .tz("America/Vancouver")
-  .format("ddd, MMM Do YYYY");
+  let vancouverDateElement = document.querySelector("#vancouver-date");
+  vancouverDateElement.innerHTML = moment()
+    .tz("America/Vancouver")
+    .format("ddd, MMM Do YYYY");
 
-let vancouverTimeElement = document.querySelector("#vancouver-time");
-vancouverTimeElement.innerHTML = moment()
-  .tz("America/Vancouver")
-  .format("hh:mm A");
+  let vancouverTimeElement = document.querySelector("#vancouver-time");
+  vancouverTimeElement.innerHTML = moment()
+    .tz("America/Vancouver")
+    .format("hh:mm:ss A");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
